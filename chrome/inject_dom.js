3924330b0f8db1f45cec3d016ecac0fca2f88bd1
Hai-Wei Wang <hwwang2005@gmail.com>
2024-10-07 17:08:03 +0800

    inject flag div: chrome_extension_sharelinkdownloader_exist

---
+++ chrome/inject_dom.js
@@ -0,0 +1,5 @@
+let tmp_div_element__ = document.createElement('div');
+tmp_div_element__.id = 'chrome_extension_sharelinkdownloader_exist';
+tmp_div_element__.style.display = 'none';
+tmp_div_element__.innerText = 'exist';
+(document.head || document.documentElement).appendChild(tmp_div_element__);
